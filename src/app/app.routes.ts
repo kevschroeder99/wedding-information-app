a1e85f322c93ddad346eed93333dab3de2c6a080
Schröder, Kevin <kevin.schroeder@hdi.de>
2024-10-31 14:08:19 +0100

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component'
+import { ScheduleComponent } from './schedule/schedule.component';
+import { LocationComponent } from './location/location.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'schedule', component: ScheduleComponent },
+    { path: 'location', component: LocationComponent },
+    { path: 'gallery', component: GalleryComponent },
+];
---
+++ src/app/gallery/gallery.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-gallery',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './gallery.component.html',
+  styleUrl: './gallery.component.css'
+})
+
+
+export class GalleryComponent {
+
+}
